bb58644a3b811a7896e60abe11bc666ec4e4a2b1
Mickaël Fleurus <fleurusmickael@gmail.com>
2020-09-29 15:59:56 +0200

    Change folder organization. Add back game intro animation.

---
+++ Assets/Scripts/UI/Game/FadingPlane/GameFadingPlane.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameFadingPlane : MonoBehaviour
+{
+    [SerializeField] private Text m_gameModeName = null; 
+    
+    public void FadeToWhiteEnded()
+    {
+        m_gameModeName.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/UI/Game/Intro/Intro.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimplePong.Localisation;
+
+public class Intro : MonoBehaviour
+{
+    private LocaliseText m_localiseText;
+    private Text m_text;
+    private int m_countdown = 3;
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+        m_localiseText = GetComponent<LocaliseText>();
+        
+        switch(GameParameters.selectedMode)
+        {
+            case GameParameters.GameMode.Cooperation:
+                m_localiseText.key = TranslationKeys.Cooperation;
+                break;
+            case GameParameters.GameMode.ScoreLimit:
+                m_localiseText.key = TranslationKeys.ScoreLimit;
+                break;
+            case GameParameters.GameMode.TimeLimit:
+                m_localiseText.key = TranslationKeys.TimeLimit;
+                break;
+        }
+    }
+
+    public void StartCountdown()
+    {
+        m_text.text = m_countdown.ToString();
+    }
+
+    
+    public void UpdateCountdown()
+    {
+        if (--m_countdown == 0)
+            m_text.text = "Go!";
+        else
+            m_text.text = m_countdown.ToString();
+    }
+}
